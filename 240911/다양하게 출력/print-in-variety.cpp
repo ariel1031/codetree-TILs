e4bf855d80a2aac03c6dfcbb089401dedd16a9d5
ariel1031 <68412350+ariel1031@users.noreply.github.com>
2024-09-11 11:50:23 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/다양하게 출력/print-in-variety.cpp
+++ 240911/다양하게 출력/print-in-variety.cpp
@@ -5,8 +5,8 @@ int main() {
     cout << "Total days in Year\n";
     cout << 365 << "\n";
     cout << "Circumference rate\n"; 
-    cout << fixed;
-    cout.precision(10);
+    cout << fixed; //cout을 사용하여 실수를 출력하면 전체 자리수(정수 부분 + 소수점 부분)가 6자리로 고정되어 출력된다.
+    cout.precision(10); //소수점 아래 10개의 수를 나타내기
     cout << 3.1415926535;
     return 0;
 }

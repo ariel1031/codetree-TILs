2b7e76c0e8d9a559938176de0ca65fdce92c8b2d
ariel1031 <68412350+ariel1031@users.noreply.github.com>
2024-09-11 11:46:30 +0900

    Title: 다양하게 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/다양하게 출력/print-in-variety.cpp
+++ 240911/다양하게 출력/print-in-variety.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
-    cout << "Total days in Year" << endl << 365 << endl << "Circumference rate" << endl << "3.1415926535";
+    cout << "Total days in Year\n";
+    cout << 365 << "\n";
+    cout << "Circumference rate\n"; 
+    cout << fixed;
+    cout.precision(10);
+    cout << 3.1415926535;
     return 0;
 }
